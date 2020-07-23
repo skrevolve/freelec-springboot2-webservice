ac290473b40a2ba9cce0f17e48ad607e1da6b7f3
sukyu.lim <sukyu.lim@purpleworks.co.kr>
2020-07-23 18:55:37 +0900

    등록버튼 기능 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,30 +1,28 @@
-var main = { //index객체 안에서만 함수가 유효하기 때문에 다른 js와 겹칠 위험이 사라진다.
-    init : function () {
-        var _this = this;
-        $('#btn-save').on('click', function() {
-            _this.save();
-        });
-    },
-    save : function () {
-        var data = {
-            title : $('#title').val(),
-            author : $('#author').val(),
-            content : $('#content').val()
-        };
+{{>layout/header}}
 
-        $.ajax({
-            type: 'POST',
-            url: '/api/v1/posts',
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 등록되었습니다.');
-            window.location.href = '/';
-        }).fail(function() {
-            alert(JSON.stringify(error));
-        })
-    }
-};
+ <h1>게시글 등록</h1>
 
-index.init();
+ <div class="col-md-12">
+     <div class="col-md-4">
+         <form>
+             <div class="form-group">
+                 <label for="title">제목</label>
+                 <input type="text" class="form-control" id="title" placeholder="제목을 입력하세요">
+             </div>
+             <div class="form-group">
+                 <label for="author"> 작성자 </label>
+                 <input type="text" class="form-control" id="author" placeholder="작성자를 입력하세요">
+             </div>
+             <div class="form-group">
+                 <label for="content"> 내용 </label>
+                 <textarea class="form-control" id="content" placeholder="내용을 입력하세요"></textarea>
+             </div>
+         </form>
+         <a href="/" role="button" class="btn btn-secondary">취소</a>
+         <button type="button" class="btn btn-primary" id="btn-save">등록</button>
+     </div>
+ </div>
+
+ {{>layout/footer}}
+
+ main.init();
