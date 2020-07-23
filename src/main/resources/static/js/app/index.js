d3b55cd1e479b8dfc01994d20a8bfba805eef081
sukyu.lim <sukyu.lim@purpleworks.co.kr>
2020-07-23 18:49:37 +0900

    등록버튼 기능 추가

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,30 @@
+var main = { //index객체 안에서만 함수가 유효하기 때문에 다른 js와 겹칠 위험이 사라진다.
+    init : function () {
+        var _this = this;
+        $('#btn-save').on('click', function() {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+            title : $('#title').val(),
+            author : $('#author').val(),
+            content : $('#content').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/posts',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function() {
+            alert(JSON.stringify(error));
+        })
+    }
+};
+
+index.init();
