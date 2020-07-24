b1109461fc107a2692a93118730e5b1bd5e49599
sukyu.lim <sukyu.lim@purpleworks.co.kr>
2020-07-24 16:15:53 +0900

    User엔티티 관련 추가

---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/User.java
@@ -0,0 +1,55 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) //Enum값이 기본적으로 int형이라 문자열로 저장되도록 한다.(무슨코드인지 알아봐야하므로)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
---
+++ src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.jojoldu.book.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findByEmail(String email); //email을 통해 이미 생성된 사용자인지 검증.
+}
