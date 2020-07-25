e3cf28ca38d5048472968c730dce75f39b9b4900
sukyu.lim <sukyu.lim@purpleworks.co.kr>
2020-07-25 21:43:44 +0900

    구글 로그인 연동 기능 추가

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -29,9 +29,11 @@ public class OAuthAttributes {
     }
 
     //OAuth2User에서 반환하는 사용자 정보는 Map이기 떄문에 값 하나하나를 변환해야한다.
-    public static OAuthAttributes of(String registrationId,
-                                           String userNameAttributeName,
-                                           Map<String, Object> attributes) {
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -42,6 +44,18 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
